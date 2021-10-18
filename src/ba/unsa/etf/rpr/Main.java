8c8b62b2c4d7be8d0d18834e184fcdac4fd363b9
amina <apandzic2@etf.unsa.ba>
2021-10-18 13:03:45 +0200

    Commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Scanner;
+
+public class Main {
+    static int sumaCifara(int n) {
+        int suma=0;
+        while(n!=0) {
+            suma+=n%10;
+            n/=10;
+        }
+        return suma;
+    }
+    public static void main(String[] args) {
+        System.out.print("Unesite broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt();
+        if(n>1) {
+            System.out.println("Brojevi djeljivi sa sumom svojih cifara su:");
+            for (int i = 2; i < n; i++) {
+                if(i%sumaCifara(i)==0) System.out.println(i);
+            }
+        }
+    }
+}
